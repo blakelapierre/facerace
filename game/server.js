e98c9eba861b359f8169b484f7a2bd4846188a5c
Blake La Pierre <blakelapierre@gmail.com>
2014-03-28 18:28:00 -0700

    this is cool :)

--- game/server.js
+++ game/server.js
@@ -3,13 +3,40 @@ var startServer = function(config, callback) {
 		socketIO = require('socket.io'),
 		webRTC = require('webrtc.io'),
 		path = require('path'),
+		otj = require('ottypes/lib/json0'),
 		app = express();
 
-	app.use(express.static(path.join(__dirname, '/public')));	
+	app.use(express.static(path.join(__dirname, '/public')));
+
+	// console.dir(otj);
+
+	// var db = otj.create({
+	// 	test: 'value',
+	// 	number: 1,
+	// 	sub: {
+	// 		child: 2
+	// 	}
+	// });
+
+	// console.dir(db);
+
+	// var db2 = otj.apply(db, [{p:['test'], od: 'value', oi: 'value2'}]);
+	// console.dir(db);
+	// console.dir(db2);
+
+	// var sub = otj.apply(db.sub, [{p:['child2'], oi: 'woah'}]);
+	// console.dir(sub);
+	// console.dir(db);
 
 	var webserver = app.listen(config.port),
 		rtc = webRTC.listen(config.rtcport),
 		io = socketIO.listen(webserver);
+
+	io.sockets.on('connection', function(socket) {
+		socket.on('watch', function(data) {
+			var path = data.path;
+		});
+	});
 
 	return callback(webserver, io, rtc);
 };
