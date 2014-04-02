f4e930d07ed847d0c5de38fbeb7f58edb2e7131b
Blake La Pierre <blakelapierre@gmail.com>
2014-04-01 17:07:28 -0700

    checking on something

--- game/public/js/facerace/facerace.js
+++ game/public/js/facerace/facerace.js
@@ -1,25 +1,19 @@
 var core = require('./core');
 
 module.exports = function(io) {
-	var eventHandlers = {
-		message: function(event) {
-			console.log(event);
-			return true;
-		}
-	};
-
-	var getEvents = function() {
-		return [{type: 'message'}];
-	};
-
-	var update = function(clock) {
-		console.log(clock);
-	};
-
 	return (function(core) {
 		return function(additionalEvents) {
 			var events = core();
 			return events;
 		};	
-	})(core(eventHandlers, getEvents, update));
+	})(core({
+		message: function(event) {
+			console.log(event);
+			return true;
+		}
+	}, function() {
+		return [{type: 'message'}];
+	}, function(clock) {
+		console.log(clock);
+	}));
 };
