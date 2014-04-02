4380e080a49aabbb7491854962815191436559e1
Blake La Pierre <blakelapierre@gmail.com>
2014-04-01 17:57:19 -0700

    I'll cleanup sometime

--- game/public/js/facerace/facerace.js
+++ game/public/js/facerace/facerace.js
@@ -1,18 +1,40 @@
-var core = require('./core');
+var _ = require('lodash'),
+	core = require('./core');
 
 module.exports = function(io) {
+	var eventQ = [];
+
+	var swapQ = function(newQ) {
+		var events = newQ || eventQ;
+		eventQ = [];
+		return events;
+	};
+
+	io.sockets.on('connection', function(socket) {
+		socket.on('position', function(data) {
+			eventQ.push({socket: socket, type: 'position', data: data});
+		});
+	});
+
 	return (function(core) {
 		return function(additionalEvents) {
 			var events = core();
+			_.each(events, function(event) {
+				event.socket.broadcast({type: event.type, data: event.data})
+			});
 			return events;
 		};	
 	})(core({
 		message: function(event) {
 			console.log(event);
 			return true;
+		},
+		position: function(event) {
+			var data = event.data;
+			console.log(data);
 		}
 	}, function() {
-		return [{type: 'message'}];
+		return swapQ();
 	}, function(clock) {
 		console.log(clock);
 	}));
