207ff97ba2bf621d01d52912fdbd2e1240c00010
Blake La Pierre <blakelapierre@gmail.com>
2014-04-17 14:07:35 -0700

    ok, pretty sure that one doesn't need to be there

--- game/public/js/facerace/facerace.js
+++ game/public/js/facerace/facerace.js
@@ -138,8 +138,7 @@ module.exports = function(isServer, rtc, io) {
 				});
 
 				socket.on('disconnect', function(data) {
-					var state = getState(),
-						event = {type: 'playerLeave', _player: player, _event: player.id};
+					var event = {type: 'playerLeave', _player: player, _event: player.id};
 					eventQ.push(event);
 				});
 			}
