97b30d249bc3192b4ef18feccdb91e85e5da5894
Blake La Pierre <blakelapierre@gmail.com>
2014-04-18 20:51:56 -0700

    stuff

--- game/public/js/hud/badge/directive.js
+++ game/public/js/hud/badge/directive.js
@@ -5,7 +5,17 @@ module.exports = function() {
 		template: require('./template.html'),
 		restrict: 'E',
 		controller: ['$scope', function($scope) {
-			
+			$scope.toggleMenu = function() {
+				$scope.showMenu = !$scope.showMenu;
+			};
+
+			$scope.$watch('showMenu', function(newValue) {
+				if (newValue) {
+					$scope.showPlus = true;
+					$scope.showAddress = false;
+				}
+				$scope.controlsEnabled = !newValue;
+			})
 		}]
 	}
 };
