8585e1b6f66a65ca0df211b9fa00e0775dc0b5e8
Blake La Pierre <blakelapierre@gmail.com>
2014-04-21 18:32:17 -0700

    more moving

--- game/src/frontend/app.js
+++ game/src/frontend/app.js
@@ -1,13 +1,18 @@
 var angular = require('angular');
 
 module.exports = angular.module('facerace', [])
-	.factory('socket', require('./inputs/socket/factory'))
-	.factory('orientation', require('./inputs/orientation/factory'))
-	.directive('focusOn', require('./util/focusOn/directive'))
-	.directive('ngEnter', require('./util/ngEnter/directive'))
-	.directive('facerace', require('./directive'))
-	.directive('scene', require('./scene/directive'))
-	.directive('hud', require('./hud/directive'))
-	.directive('badge', require('./hud/badge/directive'))
-	.directive('debug', require('./hud/debug/directive'))
-	.directive('inputs', require('./inputs/video/directive'));
+	.directive('facerace', 	require('./directive'))
+
+	.directive('hud', 		require('./hud/directive'))
+	.directive('badge', 	require('./hud/badge/directive'))
+	.directive('debug', 	require('./hud/debug/directive'))
+
+	.directive('inputs', 	require('./inputs/video/directive'))
+	
+	.directive('scene', 	require('./scene/directive'))
+	
+	.directive('focusOn', 	require('./util/focusOn/directive'))
+	.directive('ngEnter', 	require('./util/ngEnter/directive'))
+
+	.factory('socket', 		require('./inputs/socket/factory'))
+	.factory('orientation', require('./inputs/orientation/factory'));
