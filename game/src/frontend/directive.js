3d90295b2628762fc7eecff73d21d9309353febc
Blake La Pierre <blakelapierre@gmail.com>
2014-04-27 01:42:30 -0700

    will try again tomorrow

--- game/src/frontend/directive.js
+++ game/src/frontend/directive.js
@@ -89,8 +89,8 @@ module.exports = ['socket', 'keys', function FaceraceDirective (socket, keys) {
 							newKeys = _.difference(currentKeys, oldKeys),
 							removableKeys = _.difference(oldKeys, currentKeys);
 
-						_.each(newKeys, newAction);
-						_.each(removableKeys, removeAction);
+						_.each(newKeys, function(key) { newAction(key, $scope[key]); });
+						_.each(removableKeys, function(key) { removeAction(key, $scope[key]); });
 
 						var index = 0;
 						_.each(obj, function(o) {
@@ -99,142 +99,137 @@ module.exports = ['socket', 'keys', function FaceraceDirective (socket, keys) {
 					};
 				};
 
+				var addPlayer = function(key, player) {
+					var width = 1,
+						height = 1,
+						material = new THREE.MeshBasicMaterial({side: THREE.DoubleSide}),
+						mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height, 1, 1), material),
+						player = {
+							data: $scope.players[key],
+							targetQuaternion: new THREE.Quaternion(),
+							mesh: mesh,
+							material: material
+						};
+
+					scene.add(mesh);
+					mesh.visible = false;
+
+					var teaser = document.createElement('div');
+					var teaser = document.createElement('slider');
+					teaser.type = 'range'
+					// teaser.className = 'teaser';
+					// teaser.innerText = 'turn your camera on!';
+
+					var teaserObj = new THREE.CSS3DObject(teaser);
+					cssScene.add(teaserObj);
+
+					var material = new THREE.MeshBasicMaterial();
+
+					material.color.set('black');
+					material.opacity = 0;
+					material.blending = THREE.NoBlending;
+
+					var geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+					var planeMesh= new THREE.Mesh( geometry, material );
+
+					planeMesh.position.x = 0;
+					planeMesh.position.y = 0;
+					teaserObj.scale.multiplyScalar(1 / 200);
+
+					//sliderObj.position = planeMesh.position;
+					teaserObj.position.x = -2;
+					teaserObj.position.y = -2;
+					teaserObj.position.z = 0.1;
+					teaserObj.quaternion = planeMesh.quaternion;
+
+					//scene.add(planeMesh);
+
+					livePlayers[key] = player;
+				};
+
+				// http://danpearcymaths.wordpress.com/2012/09/30/infinity-programming-in-geogebra-and-failing-miserably/
+				var p = function(a) { math.floor(math.sqrt(math.add(math.multiply(4, a), 1))); };
+				var q = function(p, a) { math.subtract(a, math.floor(math.divide(math.square(p), 4))); };
+				var parser = math.parser();
+
 				var livePlayers = {};
-				$scope.$watchCollection('players', (function() {
-					// http://danpearcymaths.wordpress.com/2012/09/30/infinity-programming-in-geogebra-and-failing-miserably/
-					var p = function(a) { math.floor(math.sqrt(math.add(math.multiply(4, a), 1))); };
-					var q = function(p, a) { math.subtract(a, math.floor(math.divide(math.square(p), 4))); };
-					var parser = math.parser();
+				$scope.$watchCollection('players', createWatchCollectionFunction(livePlayers, {
+					newAction: addPlayer,
+					removeAction: function(key) {
+						var player = livePlayers[key];
+						scene.remove(player.mesh);
+						delete livePlayers[key];
+					},
+					updateAction: function(player, index) {
+						parser.eval('a = ' + index);
+						parser.eval('p = floor(sqrt(4 * a + 1))');
+						parser.eval('q = a - floor(p^2 / 4)');
+						
+						var point = parser.eval('q * i^p + (floor((p + 2) / 4) - floor((p + 1) / 4) * i) * i^(p - 1)');
 
-					return createWatchCollectionFunction(livePlayers, {
-						newAction: function(key) {
-							var width = 1,
-								height = 1,
-								material = new THREE.MeshBasicMaterial({side: THREE.DoubleSide}),
-								mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height, 1, 1), material),
-								player = {
-									data: $scope.players[key],
-									targetQuaternion: new THREE.Quaternion(),
-									mesh: mesh,
-									material: material
-								};
+						var mesh = player.mesh;
+						mesh.position.y = point.re;
+						mesh.position.x = point.im;
+					}
+				}));
 
-							scene.add(mesh);
-							mesh.visible = false;
+				var addSource = function(key, source) {
+					console.log('source', key)
+					var videoSource = $scope.sources[key],
+						video = videoSource.element,
+						width = 1,
+						height = 1,
+						texture = new THREE.Texture(video), 
+						material = new THREE.ShaderMaterial({
+							fragmentShader: document.getElementById('plane-fragment-shader' + swirl).textContent,
+							vertexShader: document.getElementById('plane-vertex-shader' + swirl).textContent,
+							uniforms: {
+								texture: {type: 't', value: texture},
+								width: {type: 'f', value: width},
+								height: {type: 'f', value: height},
+								radius: {type: 'f', value: 2},
+								angle: {type: 'f', value: 0.8},
+								center: {type: 'v2', value: new THREE.Vector2(width / 2, height / 2)},
+								time: {type: 'f', value: 1.0}
+							},
+							side: THREE.DoubleSide
+						}),
+						mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height, 1, 1), material);
 
-							var teaser = document.createElement('div');
-							var teaser = document.createElement('slider');
-							teaser.type = 'range'
-							// teaser.className = 'teaser';
-							// teaser.innerText = 'turn your camera on!';
+					texture.lastUpdate = 0;
 
-							var teaserObj = new THREE.CSS3DObject(teaser);
-							cssScene.add(teaserObj);
+					scene.add(mesh);
 
-							var material = new THREE.MeshBasicMaterial();
+					videoSource.mesh = mesh;
+					videoSource.texture = texture;
+					videoSource.material = material;
+					liveSources[key] = videoSource;
 
-							material.color.set('black');
-							material.opacity = 0;
-							material.blending = THREE.NoBlending;
-
-							var geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
-							var planeMesh= new THREE.Mesh( geometry, material );
-
-							planeMesh.position.x = 0;
-							planeMesh.position.y = 0;
-							teaserObj.scale.multiplyScalar(1 / 200);
-
-							//sliderObj.position = planeMesh.position;
-							teaserObj.position.x = -2;
-							teaserObj.position.y = -2;
-							teaserObj.position.z = 0.1;
-							teaserObj.quaternion = planeMesh.quaternion;
-
-							//scene.add(planeMesh);
-
-							livePlayers[key] = player;
-						},
-						removeAction: function(key) {
-							var player = livePlayers[key];
-							scene.remove(player.mesh);
-							delete livePlayers[key];
-						},
-						updateAction: function(player, index) {
-							parser.eval('a = ' + index);
-							parser.eval('p = floor(sqrt(4 * a + 1))');
-							parser.eval('q = a - floor(p^2 / 4)');
-							
-							var point = parser.eval('q * i^p + (floor((p + 2) / 4) - floor((p + 1) / 4) * i) * i^(p - 1)');
-
-							var mesh = player.mesh;
-							mesh.position.y = point.re;
-							mesh.position.x = point.im;
-						}
-					});
-				})());
+					if (videoSource.socketID == rtc._me) {
+						facerace.video(videoSource.socketID);
+					}
+				};
 
 				var liveSources = {};
-				$scope.$watchCollection('sources', (function() {
-					// http://danpearcymaths.wordpress.com/2012/09/30/infinity-programming-in-geogebra-and-failing-miserably/
-					var p = function(a) { math.floor(math.sqrt(math.add(math.multiply(4, a), 1))); };
-					var q = function(p, a) { math.subtract(a, math.floor(math.divide(math.square(p), 4))); };
-					var parser = math.parser();
+				$scope.$watchCollection('sources', createWatchCollectionFunction(liveSources, {
+					newAction: addSource,
+					removeAction: function(key) {
+						var videoSource = liveSources[key];
+						scene.remove(videoSource.mesh);
+						delete liveSources[key];
+					},
+					updateAction: function(source, index) {
+						parser.eval('a = ' + index);
+						parser.eval('p = floor(sqrt(4 * a + 1))');
+						parser.eval('q = a - floor(p^2 / 4)');
+						
+						var point = parser.eval('q * i^p + (floor((p + 2) / 4) - floor((p + 1) / 4) * i) * i^(p - 1)');
 
-					return createWatchCollectionFunction(liveSources, {
-						newAction: function(key) {
-							console.log('source', key)
-							var videoSource = $scope.sources[key],
-								video = videoSource.element,
-								width = 1,
-								height = 1,
-								texture = new THREE.Texture(video), 
-								material = new THREE.ShaderMaterial({
-									fragmentShader: document.getElementById('plane-fragment-shader' + swirl).textContent,
-									vertexShader: document.getElementById('plane-vertex-shader' + swirl).textContent,
-									uniforms: {
-										texture: {type: 't', value: texture},
-										width: {type: 'f', value: width},
-										height: {type: 'f', value: height},
-										radius: {type: 'f', value: 2},
-										angle: {type: 'f', value: 0.8},
-										center: {type: 'v2', value: new THREE.Vector2(width / 2, height / 2)},
-										time: {type: 'f', value: 1.0}
-									},
-									side: THREE.DoubleSide
-								}),
-								mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height, 1, 1), material);
-
-							texture.lastUpdate = 0;
-
-							scene.add(mesh);
-
-							videoSource.mesh = mesh;
-							videoSource.texture = texture;
-							videoSource.material = material;
-							liveSources[key] = videoSource;
-
-							if (videoSource.socketID == rtc._me) {
-								facerace.video(videoSource.socketID);
-							}
-						},
-						removeAction: function(key) {
-							var videoSource = liveSources[key];
-							scene.remove(videoSource.mesh);
-							delete liveSources[key];
-						},
-						updateAction: function(source, index) {
-							parser.eval('a = ' + index);
-							parser.eval('p = floor(sqrt(4 * a + 1))');
-							parser.eval('q = a - floor(p^2 / 4)');
-							
-							var point = parser.eval('q * i^p + (floor((p + 2) / 4) - floor((p + 1) / 4) * i) * i^(p - 1)');
-
-							var mesh = source.mesh;
-							mesh.position.y = point.re;
-							mesh.position.x = point.im;
-						}
-					});
-				})());
+						var mesh = source.mesh;
+						mesh.position.y = point.re;
+						mesh.position.x = point.im;
+					}
+				}));
 
 				$scope.$on('updateScene', (function() {
 					var waitingForState = function(transport, now, dt) {
