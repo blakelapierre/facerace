22bdd8523a273cd48904cc1d88436111c5b99eae
Blake La Pierre <blakelapierre@gmail.com>
2014-05-04 18:16:19 -0700

    Tween Tools

---
+++ game/src/frontend/hud/tools/tweenController/directive.js
@@ -0,0 +1,14 @@
+var Tweenable = require('tweenable');
+
+module.exports = function() {
+	return {
+		restrict: 'E',
+		template: require('./template.html'),
+		link: function($scope, element, attributes) {
+
+			$scope.$watch('tweenControlObject', function(obj) {
+
+			});
+		}
+	}
+};
